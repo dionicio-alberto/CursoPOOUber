fda090e4282a68e8fef676f366c958e4a2ed1078
Dionicio <dionicio.pl06@gmail.com>
2023-03-27 20:08:20 -0600

    Dando vida a las clases en python

--- Python/main.py
+++ Python/main.py
@@ -1,3 +1,20 @@
+from car import Car
+
+
 if __name__ == "__main__":
     print("Hello word")
+    
+    car = Car()
+    car.license = "XYZW789"
+    car.driver = "Beto Benitez"
+    car.id = 11244
+    car.passenger = 5
+    print(vars(car))
+    
+    car2 = Car()
+    car2.license = "GGH542"
+    car2.driver = "Camilia Cardenas"
+    car2.id = 11241
+    car2.passenger = 3
+    print(vars(car2))
     
