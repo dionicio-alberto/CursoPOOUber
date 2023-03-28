987549d72ac4c12a73efa0502b2dbf7674ad6b81
Dionicio <dionicio.pl06@gmail.com>
2023-03-28 10:47:25 -0600

    Creacion de objetos a partir de la superclase car en JS

---
+++ JS/UberBlack.js
@@ -0,0 +1,7 @@
+class UberBlack extends Car {
+    constructor(license, driver, typeCarAccepted, seatsMaterial) ´{
+        super(license,driver)
+        this.typeCarAccepted    = typeCarAccepted;
+        this.seatsMaterial      = seatsMaterial;
+    }
+}
--- JS/index.js
+++ JS/index.js
@@ -2,3 +2,6 @@ var car = new Car("ABC1998",new Account("Daniel Dominguez","DOXD981106HTC"))
 car.passenger = 4;
 car.printDataCar()
 
+var uberX = new UberX("AW456", new Account("Andrea Ferran", "ANDA765"), "Chevrolet", "Spark")
+uberX.passenger = 4;
+uberX.printDataCar();
