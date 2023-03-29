0feab0284431147d1c7ffb11f740f47c8d7119cc
Dionicio <dionicio.pl06@gmail.com>
2023-03-28 18:33:00 -0600

    Creacion de subclases del tipo payment en java

--- Java/Account.java
+++ Java/Account.java
@@ -1,12 +1,25 @@
-class Account {
+class Account{
     Integer id;
-    String name;
-    String document;
-    String email;
-    String password;
+    String name;    
+    String document;    
+    String email;    
+    String password;    
+    
+    public Account(Integer id, String name, String document,  String email, String password){
+        this.id         = id;
+        this.name       = name;
+        this.document   = document;
+        this.email      = email;
+        this.password   = password;
+    }
 
-    public Account(String name, Integer id){
-        this.name = name;
-        this.id = id;
+    void printDataAccount(){
+        System.out.println("---User Data--");
+        System.out.println("id: " + id);
+        System.out.println("name: " + name);
+        System.out.println("document: " + document);
+        System.out.println("email: " + email);
+        System.out.println("password: " + password);
+        System.out.println("-------------");
     }
 }
--- Java/main.java
+++ Java/main.java
@@ -2,11 +2,11 @@ class Main {
     public static void main(String[] args) {
         System.out.println("hola mundo");
         
-        Car car = new Car("ABC123", new Account("Andres Herrera", 0001));
+        Car car = new Car("ABC123", new Account(1111,"Andres Herrera", "0001","correo@dfjs","dasfasd"));
         car.passenger = 4;
         car.printDataCar();
 
-        Car car2 = new Car("DEF456",new Account("Andrea Herrera", 0002));
+        Car car2 = new Car("DEF456",new Account(1311,"Ansadres Herrera", "0002","correo2@dfjs","dasfadsd"));
         car2.passenger = 3;
         car2.printDataCar();
     }
