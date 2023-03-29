7efb1db58d30dc43c050736cfdcb7276829117d9
Dionicio <dionicio.pl06@gmail.com>
2023-03-28 18:17:05 -0600

    Creacion de subclases a partir de la superclase payment en el lenguaje PHP

--- PHP/Payment.php
+++ PHP/Payment.php
@@ -1,5 +1,10 @@
 <?php
 class Payment {
     public $id;
+
+    public function __construct($id){
+        $this->id = $id;
+    }
+
 }
 ?>
--- PHP/index.php
+++ PHP/index.php
@@ -3,10 +3,17 @@ require_once("car.php");
 require_once("uberX.php");
 require_once("uberPool.php");
 require_once("account.php");
+require_once('payment.php');
+require_once('card.php');
+
 
 $uberX = new UberX("CVB123", new Account("Andres Herrera", "AND456"), "Chevrolet", "Spark");
 $uberX->printDataCar();
 
 $uberPool = new UberPool("TYU567", new Account("Andrea Ferran", "ANDA765"), "Dodge", "Attitude");
 $uberPool->printDataCar();
+
+$card = new Card(199, 1234657890, 123, "08/25");
+
+$card->printDataCard();
 ?>
